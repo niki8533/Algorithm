ed09fa7b8dd620f8479fe2e2853f37fda1c67b53
niki8533 <niki8533@naver.com>
2025-03-26 18:49:25 +0900

    feat: 프로그래머스 완주하지 못한 선수 풀이

---
+++ src/main/java/Hash/완주하지_못한_선수.java
@@ -0,0 +1,33 @@
+package Hash;
+
+import java.util.*;
+
+public class 완주하지_못한_선수 {
+    public static void main(String[] args) {
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+        String answer = "";
+
+        //처음에 list로 해당 문제를 풀었었는데
+        //시간이 오버가 되면서 hashMap으로 문제를 풀었다
+        //다른 방법이 있나 찾아보고 싶은데..
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(int i = 0 ; i < participant.length ; i++){
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+        }
+
+        for(int i = 0 ; i < completion.length ; i++){
+            map.put(completion[i], map.getOrDefault(completion[i], 0) + 1);
+        }
+
+        for(String name: participant){
+            if(map.get(name) % 2 != 0){
+                answer = name;
+                break;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
